98ad6a3cb50abb5c496ef2ee3e94db8c1d4c7fed
stephen447 <byrnes67@tcd.ie>
2025-03-11 17:05:41 +0000

    set up eslint and src folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <p className="text-amber-300"> Hello World</p>;
+  return <p className="text-amber-300">Hello World</p>;
 }
