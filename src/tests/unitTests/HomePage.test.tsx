50e1f3531338a14720560a1e2f54f3e2b173ddd5
stephen447 <byrnes67@tcd.ie>
2025-03-22 14:10:01 +0000

    home page unit test refactor

---
+++ src/tests/unitTests/HomePage.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { RelayEnvironmentProvider } from "react-relay/hooks";
+import { createMockEnvironment } from "relay-test-utils";
+import HomePage from "../../app/page";
+import { useRouter } from "next/navigation";
+
+// Mock useRouter
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+}));
+
+// Mocking the PopularCryptoList to ensure it renders correctly (Separate unit tests for this component)
+jest.mock("../../components/PopularTokenList", () => ({
+  __esModule: true,
+  default: () => <div>Popular Cryptos Mocked</div>,
+}));
+
+describe("Home Page unit tests", () => {
+  let mockEnvironment: any;
+  let mockPush: jest.Mock;
+
+  beforeEach(() => {
+    mockEnvironment = createMockEnvironment();
+    mockPush = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({ push: mockPush });
+  });
+
+  it("Renders Homepage with the Header and search bar", () => {
+    render(
+      <RelayEnvironmentProvider environment={mockEnvironment}>
+        <HomePage />
+      </RelayEnvironmentProvider>
+    );
+
+    expect(screen.getByText(/Crypto Sphere/i)).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText(/Search cryptocurrency/i)
+    ).toBeInTheDocument();
+  });
+
+  it("redirects to search page when a search is performed", () => {
+    render(
+      <RelayEnvironmentProvider environment={mockEnvironment}>
+        <HomePage />
+      </RelayEnvironmentProvider>
+    );
+
+    const input = screen.getByPlaceholderText(/Search cryptocurrency/i);
+    const button = screen.getByRole("button", { name: /search/i });
+
+    fireEvent.change(input, { target: { value: "Bitcoin" } });
+    fireEvent.click(button);
+
+    expect(mockPush).toHaveBeenCalledWith("/search?query=Bitcoin");
+  });
+});
