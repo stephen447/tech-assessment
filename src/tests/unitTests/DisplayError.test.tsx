5cacda7bc86f9ee3bcd07cc28236e25eb90a4aef
stephen447 <byrnes67@tcd.ie>
2025-03-24 18:00:08 +0000

    display error test

---
+++ src/tests/unitTests/DisplayError.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import DisplayError from "../../components/DisplayError"; // Adjust the path accordingly
+import "@testing-library/jest-dom";
+
+describe("DisplayError Component", () => {
+  it("renders the error message correctly", () => {
+    render(<DisplayError />);
+
+    // Check if the error message is displayed
+    expect(
+      screen.getByText("Error loading data. Please try again.")
+    ).toBeInTheDocument();
+  });
+});
